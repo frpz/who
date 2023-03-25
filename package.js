d1c5d6fc70d4e02631e03f5ccb76be9caba48da4
Francis Pugnère <francis@frpz.fr>
2023-03-25 22:39:18 +0100

    remove : (for windows compatibility)

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'frpz:who',
   summary: "Allows you to see who's on your Meteor app",
-  version: '0.1.5',
+  version: '0.1.7',
   git: "https://github.com/frpz/who.git"
 });
 
@@ -11,13 +11,13 @@ Package.onUse(function(api) {
   api.use('iron:router@1.2.0',{weak: true});
   api.versionsFrom('2.7.1');
   api.export("frpzWho");
-  api.addFiles('frpz:who.html', 'client');
-  api.addFiles('frpz:who-client.js', 'client');
-  api.addFiles('frpz:who-server.js', 'server');
+  api.addFiles('frpz-who.html', 'client');
+  api.addFiles('frpz-who-client.js', 'client');
+  api.addFiles('frpz-who-server.js', 'server');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('frpz:who');
-  api.addFiles('frpz:who-tests.js');
+  api.addFiles('frpz-who-tests.js');
 });
