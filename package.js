af906bf4782ef4ebdd33d1b007ae44c2df642d8b
Francis Pugnère <francis@frpz.fr>
2023-03-25 21:34:19 +0100

    passage en 0.1.5

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'frpz:who',
   summary: "Allows you to see who's on your Meteor app",
-  version: '0.1.4',
+  version: '0.1.5',
   git: "https://github.com/frpz/who.git"
 });
 
 Package.onUse(function(api) {
-  api.use('standard-app-packages');
-  api.use('accounts-password',{weak: true});
-  api.use('iron:router@1.0.9',{weak: true});
-  api.versionsFrom('METEOR@0.9.0');
+  api.use('standard-app-packages@1.0.9');
+  api.use('accounts-password@2.3.1',{weak: true});
+  api.use('iron:router@1.2.0',{weak: true});
+  api.versionsFrom('2.7.1');
   api.export("frpzWho");
   api.addFiles('frpz:who.html', 'client');
   api.addFiles('frpz:who-client.js', 'client');
